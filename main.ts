6f7a724f559386481715d5006c1eeff746666044
Mathijs Blokland <1050030+mathijsblokland@users.noreply.github.com>
2025-05-26 13:44:12 +0200

    main.ts, main.blocks bijwerken

--- main.ts
+++ main.ts
@@ -1,19 +1,49 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(vochtigheid)
+})
+input.onButtonPressed(Button.B, function () {
+    if (music.volume() == 0) {
+        music.setVolume(127)
+        basic.showLeds(`
+            . . . . #
+            . . . # #
+            . . # # #
+            . # # # #
+            # # # # #
+            `)
+    } else {
+        music.setVolume(0)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            # # # # #
+            . . . . .
+            . . . . .
+            `)
+    }
+})
 let vochtigheid = 0
 led.setBrightness(64)
-basic.forever(function () {
-    pins.digitalWritePin(DigitalPin.P1, 1)
-    basic.pause(1)
-    vochtigheid = pins.analogReadPin(AnalogPin.P0)
-    pins.digitalWritePin(DigitalPin.P1, 0)
-    led.plotBarGraph(
-    vochtigheid,
-    1023
-    )
-    if (input.buttonIsPressed(Button.A)) {
-        basic.showNumber(vochtigheid)
-    }
-    basic.pause(5000)
-})
+music.setVolume(127)
 basic.forever(function () {
 	
+})
+basic.forever(function () {
+    vochtigheid = pins.analogReadPin(AnalogPin.P0)
+    if (vochtigheid > 1010) {
+        basic.showLeds(`
+            . . # # .
+            . # # . .
+            # # # . #
+            # # # # #
+            # # # # #
+            `)
+    } else if (vochtigheid > 850) {
+        basic.showIcon(IconNames.Happy)
+    } else {
+        basic.showIcon(IconNames.Confused)
+        music.play(music.tonePlayable(523, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+        music.play(music.tonePlayable(392, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    }
+    basic.pause(5000)
 })
